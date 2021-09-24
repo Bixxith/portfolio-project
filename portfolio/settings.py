1c2d05d1fa9b994cbbd7cbfced10581352c5b90e
Bixxith <39364622+Bixxith@users.noreply.github.com>
2021-09-23 21:52:20 -0400

    initial

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zachniehoff.herokuapp.com','localhost']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobs',
+    'herokuapp',
 ]
 
 MIDDLEWARE = [
