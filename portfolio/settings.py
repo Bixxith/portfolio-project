3004aa7960c505a4d5e175fc4e6f7654d9efd4c5
Bixxith <39364622+Bixxith@users.noreply.github.com>
2021-11-02 20:25:09 -0400

    Testing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://zachniehoff.herokuapp.com/','127.0.0.1','zachniehoff.com']
+ALLOWED_HOSTS = ['*']
+#'https://zachniehoff.herokuapp.com/','127.0.0.1','zachniehoff.com'
 
 
 # Application definition
