cf06c2a1d2912d8082eade40cfafbfea2ffb2c6f
Bixxith <39364622+Bixxith@users.noreply.github.com>
2021-11-02 21:46:54 -0400

    Files were being saved in a non-existent directory.
    They were being saved to media/ which didn't exist.  Rectified by switching directory to media/'s parent which was the main directory.

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Job(models.Model):
-    image = models.ImageField(upload_to='jobs/static/')
+    image = models.ImageField(upload_to='../jobs/static/')
     summary = models.CharField(max_length=200,default='')
     story = models.CharField(max_length=500, default='')
     title = models.CharField(max_length=50, default='')
